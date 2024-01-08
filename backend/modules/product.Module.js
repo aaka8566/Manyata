2202f1b1ab8c807a2c0a1c792c4afdeef89fd7ec
Aakash Chakravarty <112715657+aaka8566@users.noreply.github.com>
2024-01-08 12:15:57 +0530

    route and user schema changes for populating

--- backend/modules/product.Module.js
+++ backend/modules/product.Module.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
+const {Schema}=mongoose;
 const productSchema=mongoose.Schema({
 rating:Number,
 rating_Count:String,
--- backend/modules/user.Module.js
+++ backend/modules/user.Module.js
@@ -1,4 +1,5 @@
 const mongoose=require("mongoose");
+const {Schema}=mongoose;
 const userSchema=mongoose.Schema({
     name:String,
     username:String,
